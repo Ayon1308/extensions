ca1e458a53ebf54d1054cd9493eb13165a76bb2d
Ayon <snkumarroy@gmail.com>
2024-05-05 21:41:40 +0530

    This is a simple project for joke Generator

---
+++ Joke Generator/script.js
@@ -0,0 +1,12 @@
+const getJokes = async () => {
+    try {
+        const res = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single");
+        const data = await res.json();
+        const myJoke = document.querySelector("#myJoke");
+        myJoke.innerHTML = data.joke;
+    }catch (error) {}
+};
+
+window.addEventListener("load", () => {
+    getJokes();
+});
